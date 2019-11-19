687bc7acafcdd14454ade855e8c49f84e4e2ffde
Michael Peacock <peacoc17@coventry.ac.uk>
2019-11-19 15:09:08 +0000

    refactoring email

--- modules/email.js
+++ modules/email.js
@@ -1,30 +1,43 @@
+'use strict'
+
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 
 const transporter =nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: "coventry4c@gmail.com",
-        pass: "groupCV4"
-    }
+	service: 'gmail',
+	auth: {
+		user: "coventry4c@gmail.com",
+		pass: "groupCV4"
+	}
 });
 
-/*const takeParameters = (from,to,output) =>{
-       return  mailOption =  {
-        from: from,	
-        to: to,
-        subject: 'Student CVs',
-        text: 'example',
-        html: output 	 
-      };
-}*/
-
+function emailSetup(emailFrom, emailTo,data) {
+	const output = `<p>You have a new contact request.</p>
+	<h3>Contact Details</h3>
+	<ul><li>Name: ${data.name}</li>  
+		<li>Company: ${data.company}</li>
+		<li>Email: ${data.email}</li>
+		<li>Phone: ${data.phone}</li>
+	</ul>
+	<h3>Message</h3>
+	<p>${data.message}</p>`
+	
+	const mailOption =  {
+		from: emailFrom,	
+		to: emailTo,
+		subject: 'Student CVs',
+		text: 'example',
+		html: output
+	}
+	return mailOption
+}
 
 
 module.exports =  {
-        transporter
-        //takeParameters,
-        //mailOption
+	transporter,
+	emailSetup
+	//takeParameters,
+	//mailOption
         
 }
