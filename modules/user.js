8f876bf6034b7a8935cbd5ec4c453eee1c48585e
emrulovd <you@example.com>
2019-11-24 22:00:07 +0000

    Merge branch 'master' of https://github.coventry.ac.uk/peacoc17/Student-CV

--- modules/user.js
+++ modules/user.js
@@ -12,7 +12,7 @@ module.exports = class User {
 		return (async() => {
 			this.db = await sqlite.open(dbName)
 			// eslint-disable-next-line max-len
-			const sql = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, email TEXT, pass TEXT);'
+			const sql = 'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, email TEXT, pass TEXT, phoneNumber TEXT);'
 			await this.db.run(sql)
 			return this
 		})()
@@ -20,10 +20,10 @@ module.exports = class User {
 
 	async register(user, email, pass) {
 		try {
-			if(user.length === 0) throw new Error('missing username')
-			if(email.length === 0) throw new Error('missing email')
-			if(pass.length === 0) throw new Error('The password is missing.')
-			if(pass.length < 6) throw new Error('password is too short')
+			if (user.length === 0) throw new Error('missing username')
+			if (email.length === 0) throw new Error('missing email')
+			if (pass.length === 0) throw new Error('The password is missing.')
+			if (pass.length < 6) throw new Error('password is too short')
 			let sql = `SELECT COUNT(id) as records FROM users WHERE user="${user}";`
 			const data = await this.db.get(sql)
 			if (data.records !== 0) throw new Error(`username "${user}" already in use`)
@@ -47,11 +47,11 @@ module.exports = class User {
 		try {
 			let sql = `SELECT count(id) AS count FROM users WHERE user="${username}";`
 			const records = await this.db.get(sql)
-			if(!records.count) throw new Error(`Username "${username}" not found`)
+			if (!records.count) throw new Error(`Username "${username}" not found`)
 			sql = `SELECT id, pass FROM users WHERE user = "${username}";`
 			const record = await this.db.get(sql)
 			const valid = await bcrypt.compare(password, record.pass)
-			if(valid === false) throw new Error(`Invalid password for account "${username}"`)
+			if (valid === false) throw new Error(`Invalid password for account "${username}"`)
 			return record.id
 		} catch (err) {
 			throw err
